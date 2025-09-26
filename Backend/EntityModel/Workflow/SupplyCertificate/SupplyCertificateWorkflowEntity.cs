18d878793b25714c761360ad3cdcebb1e24a008f
Patrick Engelberts <Patrick.Engelberts@it-titans.de>
2025-09-26 20:55:21 +0200

    added review comment to SupplyCertificateWorkflowEntity

--- Backend/EntityModel/Workflow/SupplyCertificate/SupplyCertificateWorkflowEntity.cs
+++ Backend/EntityModel/Workflow/SupplyCertificate/SupplyCertificateWorkflowEntity.cs
@@ -1,5 +1,7 @@
 ﻿using ITTitans.Hackathon2025.EntityModel.Auth;
 using ITTitans.Hackathon2025.Model.Workflow.SupplyCertificate;
+using ITTitans.Hackathon2025.Utils;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ITTitans.Hackathon2025.EntityModel.Workflow.SupplyCertificate;
@@ -18,6 +20,9 @@ public class SupplyCertificateWorkflowEntity
     public HackathonUserEntity? Reviewer { get; set; }
     
     public Guid? ReviewerId { get; set; }
+
+    [StringLength(StringLengths.Comment)]
+    public string? ReviewComment { get; set; }
 
     public DateTimeOffset Created { get; set; }
 
