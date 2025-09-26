ac3b57e5763d9557f4339e5c483342b3cf11fee1
lara <lara@team-quantum.org>
2025-09-26 16:14:47 +0200

    feat(frontend): Add dummy login

---
+++ frontend/src/services/auth.ts
@@ -0,0 +1,31 @@
+﻿import type { AuthResult, Credentials } from '@/types'
+
+const mockLoginTime = (duration: number) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true)
+    }, duration)
+  })
+}
+
+export const login = async (credentials: Credentials): Promise<AuthResult> => {
+  await mockLoginTime(2500)
+
+   // TODO
+  if (credentials.email === 'lara@local.dev')
+  {
+    return {
+      token: 'i-bims-1-jwt-töken',
+      user: {
+        id: 1,
+        name: 'Lara Test',
+        email: credentials.email
+      }
+    }
+  }
+
+  return {
+    token: undefined,
+    user: undefined
+  }
+}
