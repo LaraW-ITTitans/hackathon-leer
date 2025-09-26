da16c645f4838012a7fb594b158e2ae0c7cfa92c
Patrick Engelberts <Patrick.Engelberts@it-titans.de>
2025-09-26 14:07:11 +0200

    add display name to user

---
+++ Backend/Utils/StringLengths.cs
@@ -0,0 +1,11 @@
+﻿namespace ITTitans.Hackathon2025.Utils;
+
+public static class StringLengths
+{
+    public const int UserName = 20;
+    public const int Name = 100;
+    public const int Number = 100;
+    public const int MailAddress = 250;
+    public const int Description = 2500;
+    public const int Comment = 2500;
+}
