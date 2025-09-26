b837d4597db76c582aad8bb7f2aee477ca2f61ff
lara <lara@team-quantum.org>
2025-09-26 16:13:53 +0200

    feat(frontend): Create layout

---
+++ frontend/src/components/layout/PageHeaderComponent.vue
@@ -0,0 +1,30 @@
+﻿<template>
+  <el-row>
+    <el-col :span="20"></el-col>
+    <el-col :span="4" class="quick-actions">
+      <el-button v-if="!isAuthenticated" :icon="Key" plain @click="onLoginClick"/>
+      <ThemeToggle />
+    </el-col>
+  </el-row>
+</template>
+
+<script setup lang="ts">
+import { Key } from '@element-plus/icons-vue'
+import ThemeToggle from '@/components/theme-toggle/ThemeToggleComponent.vue'
+import router from '@/router'
+import useUserStore from '@/stores/user.ts'
+
+const { isAuthenticated } = useUserStore();
+
+const onLoginClick = () => {
+  router.push('/auth');
+};
+
+</script>
+
+<style scoped>
+.quick-actions {
+  margin-left: auto;
+  flex-basis: unset;
+}
+</style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,22 @@
 import './assets/main.css'
 
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+
 import { createApp } from 'vue'
+
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const piniaStore = createPinia()
+piniaStore.use(piniaPluginPersistedstate)
+app.use(piniaStore)
+
 app.use(router)
 
 app.mount('#app')
