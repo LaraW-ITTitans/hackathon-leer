e7f27dba48631a44d859336024505b493eaea95b
lara <lara@team-quantum.org>
2025-09-27 12:00:07 +0200

    feat(frontend): Add proper favicon and page titles to app

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,7 +2,9 @@ import {
   createRouter,
   createWebHistory
 } from 'vue-router'
+
 import { authGuard } from './navigation-guard'
+import { afterEach } from './page-title.ts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,19 +18,19 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/landing/LandingView.vue'),
-      meta: { public: true },
+      meta: { public: true, title: 'Landing Page' },
     },
     {
       path: '/auth',
       name: 'login',
       component: () => import('../views/auth/LoginView.vue'),
-      meta: { public: true },
+      meta: { public: true, title: 'Login' },
     },
     {
       path: '/home',
       name: 'home',
       component: () => import('../views/home/HomeView.vue'),
-      meta: { public: true },
+      meta: { public: true, title: 'Home' },
     },
 
     // Admin Pages
@@ -36,7 +38,7 @@ const router = createRouter({
       path: '/admin/roles',
       name: 'role-management',
       component: () => import('../views/roles/RoleOverviewView.vue'),
-      meta: { public: false },
+      meta: { public: false, title: 'Administration - Rollen' },
     },
 
     // User Pages
@@ -44,18 +46,19 @@ const router = createRouter({
       path: '/users/me',
       name: 'user-profile',
       component: () => import('../views/user/profile/UserProfileView.vue'),
-      meta: { public: false },
+      meta: { public: false, title: 'Eigenes Profil' },
     },
 
     // last but not least
     {
       path: '/:catchAll(.*)*',
       component: () => import('../views/NotFoundView.vue'),
-      meta: { public: true },
+      meta: { public: true, },
     }
   ],
 })
 
-router.beforeEach(authGuard);
+router.beforeEach(authGuard)
+router.afterEach(afterEach)
 
 export default router
