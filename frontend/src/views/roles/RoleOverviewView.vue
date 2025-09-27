b49c35822041818428a355cbe771e0b9405170fa
lara <lara@team-quantum.org>
2025-09-27 13:03:53 +0200

    fix(frontend): Fix naming error in module

--- frontend/src/views/roles/RoleOverviewView.vue
+++ frontend/src/views/roles/RoleOverviewView.vue
@@ -1,5 +1,5 @@
 ﻿<script setup lang="ts">
-import RoleListComponent from '@/modules/roles-admin/RoleListComponent.vue'
+import RoleListComponent from '@/modules/roles-admin/RoleListModule.vue'
 </script>
 
 <template>
